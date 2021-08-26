a16b2fc887e50d0605e9875ab97f430a02472b85
baptLho <blhommelet@giesalsa.fr>
2021-08-26 17:26:45 +0200

    ok

--- FORM.php
+++ FORM.php
@@ -12,28 +12,53 @@ class Form
    */
   private $data;
 
+  /**
+   * @var string tag utilisé pour entourer les champs
+   */
   public $surround = 'div';
 
+  /**
+   * @param array $data utilisées par le formulaire
+   */
   public function __construct($data = [])
   {
     $this->data = $data;
   }
+
+
+  /**
+   * @param $html string code html à entourer
+   * @return string
+   */
   private function surround($html)
   {
     return "<{$this->surround}>{$html}</$this->surround}>";
   }
 
+  /**
+   * @param $index string index de la valeur à récupérer
+   * @return string
+   */
   private function getValue($index)
   {
     return isset($this->data[$index]) ? $this->data[$index] : null;
   }
 
+  /**
+   * @param $name stringr
+   * @return string
+   */
   public function input($name)
   {
     return $this->surround(
       '<input type="text" name="' . $name . '" value="' . $this->getValue($name) . '">'
     );
   }
+
+  /**
+ 
+   * @return string
+   */
   public function submit()
   {
     return $this->surround("<button type='submit'>Envoyer</button>");
