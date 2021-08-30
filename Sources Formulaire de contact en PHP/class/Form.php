afc9930e1183730f3b12ed3ae9c20f045e0b9ffe
baptLho <blhommelet@giesalsa.fr>
2021-08-30 16:54:31 +0200

    Form

--- Sources Formulaire de contact en PHP/class/Form.php
+++ Sources Formulaire de contact en PHP/class/Form.php
@@ -1,25 +1,30 @@
 <?php
-class Form {
+class Form
+{
 
     private $datas = [];
 
-    public function __construct($datas = []){
+
+    public function __construct($datas = [])
+    {
         $this->datas = $datas;
     }
 
-    private function getValue($name){
-        if(isset($this->datas[$name])) {
+    private function getValue($name)
+    {
+        if (isset($this->datas[$name])) {
             return $this->datas[$name];
-        }else{
+        } else {
             return '';
         }
     }
 
-    private function input($type, $name, $label){
+    private function input($type, $name, $label)
+    {
         $value = $this->getValue($name);
-        if($type == 'textarea'){
+        if ($type == 'textarea') {
             $input = "<textarea name=\"$name\" class=\"form-control\" id=\"input$name\">$value</textarea>";
-        }else{
+        } else {
             $input = "<input type=\"$type\" name=\"$name\" class=\"form-control\" id=\"input$name\" value=\"$value\">";
         }
         return "<div class=\"form-group\">
@@ -28,20 +33,23 @@ class Form {
         </div>";
     }
 
-    public function text($name, $label){
+    public function text($name, $label)
+    {
         return $this->input('text', $name, $label);
     }
 
-    public function email($name, $label){
+    public function email($name, $label)
+    {
         return $this->input('email', $name, $label);
     }
 
-    public function select($name, $label, $options){
+    public function select($name, $label, $options)
+    {
         $options_html = "";
         $value = $this->getValue($name);
-        foreach($options as $k => $v){
+        foreach ($options as $k => $v) {
             $selected = '';
-            if($value == $k){
+            if ($value == $k) {
                 $selected = ' selected';
             }
             $options_html .= "<option value=\"$k\"$selected>$v</option>";
@@ -52,12 +60,13 @@ class Form {
         </div>";
     }
 
-    public function textarea($name, $label){
+    public function textarea($name, $label)
+    {
         return $this->input('textarea', $name, $label);
     }
 
-    public function submit($label){
+    public function submit($label)
+    {
         return '<button type="submit" class="btn btn-primary">' . $label . '</button>';
     }
-
-} 
+}
